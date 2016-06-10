80959fc5fc9b284cb9c4cf1ddc58d54a288dbd43
kiskinvlad <kiskinvlad@gmail.com>
2016-06-10 16:01:19 +0300

    update angular version & create top-head

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -11,6 +11,34 @@ function AppCtrl($scope, $http) {
     $scope.name = 'Error!'
   });
 }
+function MultiLangCtrl($scope, $log) {
+    $scope.langs = [
+        {id: 0, lang: 'English'},
+        {id: 1, lang: 'Ukraine'},
+        {id: 2, lang: 'Russian'}
+    ];
+    $scope.currentSelect = $scope.langs[0].lang;
+    $scope.status = {
+        isopen: false
+    };
+
+    $scope.toggled = function(open) {
+        $log.log('Dropdown is now: ', open);
+    };
+    $scope.setChoice = function(choice) {
+        $scope.currentSelect = choice;
+    };
+
+    $scope.toggleDropdown = function($event) {
+        $event.preventDefault();
+        $event.stopPropagation();
+        $scope.status.isopen = !$scope.status.isopen;
+    };
+
+    $scope.appendToEl = angular.element(document.querySelector('#dropdown-long-content'));
+}
+MultiLangCtrl.$inject = ['$scope', '$log'];
+angular.module('myApp', ['ui.bootstrap']).controller('MultiLangCtrl', MultiLangCtrl);
 
 function MyCtrl1() {}
 MyCtrl1.$inject = [];
